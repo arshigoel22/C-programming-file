24226968a04afbadc91012a2e2afb4f53c4f8052
arshigoel22 <68539256+arshigoel22@users.noreply.github.com>
2021-01-04 18:24:07 +0530

    Add files via upload

---
+++ Odd Even.c
@@ -0,0 +1,24 @@
+/*Program to compare two numbers
+Prepared by - Arshi Goel
+Date - 4/Jan/2021
+*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+clrscr();
+int num;
+printf(“ Enter a number :"); 
+scanf(“%d”,&num);
+if (num % 2  == 0)
+{
+printf(“ Even Number");
+}
+else
+{
+printf("Odd Number");
+
+}
+getch();
+
+}
