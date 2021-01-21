aea0a9c7917b626e684b61d9513b8b0bf3b1e93d
arshigoel22 <68539256+arshigoel22@users.noreply.github.com>
2021-01-21 19:14:45 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int radius;
+    float pi=3.14,area,ci;
+    clrscr();
+    printf("Enter the Radius of a Circle:");
+    scanf("%d",&radius);
+    area=pi*radius*radius;
+    printf("Area of a Circle is: %f\n",area);
+    ci=2*pi*r;
+    printf("Circumference is: %f",ci);
+    getch();
+}
