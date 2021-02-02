6cab2ae64e1b3877b462a0f30acf8f0da23c639f
arshigoel22 <68539256+arshigoel22@users.noreply.github.com>
2021-02-02 18:46:13 +0530

    Add files via upload

---
+++ Palindrome.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+
+int CheckPalindrome(int n)
+{
+   
+int newNum=0,n1;
+    
+n1=n;
+    
+while(n1!=0)
+{
+        
+newNum=newNum*10+n1%10;
+        
+n1=n1/10;
+    
+}
+    
+if(newNum==n)
+{
+        
+printf("%d is Palindrome",n);
+    
+}
+    
+else{
+        
+printf("%d is not Palindrome",n);
+    
+}
+    
+return 0;
+
+}
+ 
+int main()
+ 
+{
+     
+int num;
+     
+printf("Entet Number:");
+     
+scanf("%d",&num);
+     
+CheckPalindrome(num);
+     
+
+    
+return 0;
+
+}
