453cab898234563425598e8fd2284a67d02e85fd
arshigoel22 <68539256+arshigoel22@users.noreply.github.com>
2021-02-09 10:11:11 +0530

    Add files via upload

---
+++ Union.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main()
+
+{
+    
+union item
+    
+{
+        
+int x;
+        
+char ch;
+        
+float m;
+    
+};
+    
+union item I1;
+    
+I1.x=10;
+    
+printf("%d",I1.x);
+    
+I1.ch='A';
+    
+printf("%c",I1.ch);
+    
+I1.m=15.25;
+    
+printf("%f",I1.m);
+
+    
+return 0;
+
+}
